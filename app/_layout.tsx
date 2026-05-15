2bc155a51abbe36799f94e29446afaa7fe43e97a
Recillah Khamala <recillahk@gmail.com>
2026-05-15 23:41:05 +0300

    Fix color scheme warning on web

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,15 @@ import '../global.css';
 
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.StyleSheet) {
+      (window.StyleSheet as any).setFlag?.('darkMode', 'class');
+    }
+  }, []);
+
   return (
     <GestureHandlerRootView className="flex-1">
       <Stack>
